46b37427a7c92112932081ddb7e030f4cd88acac
Yuuki K <yuuki3327@hotmail.com>
2017-05-23 23:03:40 -0700

    First tutorial done

--- demo_file.js
+++ demo_file.js
@@ -1,21 +1,62 @@
-var http = require('http');
+// var http = require('http');
+// var formidable = require('formidable');
+// var fs = require('fs');
 
-http.createServer(function(req, res){
-    res.writeHead(200, {'Content-Type' : 'text/html'});
+
+// http.createServer(function (req, res) {
+//     if (req.url == '/fileupload') {
+//         var form = new formidable.IncomingForm();
+//         form.parse(req, function (err, fields, files) {
+//             // var oldpath = files.filetoupload.path;
+//             // var newpath = '~/Users/yuukikuroshima/Google Drive/Working Folder/FirstNodeJS' + files.filetoupload.name;
+//             // fs.rename(oldpath, newpath, function(err){
+//             //     if(err) throw err;
+//             //     res.write('File uploaded and moved');
+//             //     res.end();
+//             // });
+//             var oldpath = files.filetoupload.path;
+//             var newpath = 'C:/Users/Your Name/' + files.filetoupload.name;
+//             fs.rename(oldpath, newpath, function (err) {
+//                 if (err) throw err;
+//                 res.write('File uploaded and moved!');
+//                 res.end();
+//             });
+//             res.write('File uploaded');
+//             res.end();
+//         });
+//     } else {
+//         res.writeHead(200, { 'Content-Type': 'text/html' });
+//         res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
+//         res.write('<input type="file" name="filetoupload"><br>');
+//         res.write('<input type="submit">');
+//         res.write('</form>');
+//         return res.end();
+//     }
+
+// }).listen(8080);
+
+var http = require('http');
+var formidable = require('formidable');
+var fs = require('fs');
+
+http.createServer(function (req, res) {
+  if (req.url == '/fileupload') {
+    var form = new formidable.IncomingForm();
+    form.parse(req, function (err, fields, files) {
+      var oldpath = files.filetoupload.path;
+      var newpath = '~/Users/yuukikuroshima/Google Drive/Working Folder/FirstNodeJS' + files.filetoupload.name;
+      fs.rename(oldpath, newpath, function (err) {
+        if (err) throw err;
+        res.write('File uploaded and moved!');
+        res.end();
+      });
+ });
+  } else {
+    res.writeHead(200, {'Content-Type': 'text/html'});
     res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
     res.write('<input type="file" name="filetoupload"><br>');
     res.write('<input type="submit">');
     res.write('</form>');
     return res.end();
+  }
 }).listen(8080);
-
-// var http = require('http');
-
-// http.createServer(function (req, res) {
-//   res.writeHead(200, {'Content-Type': 'text/html'});
-//   res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
-//   res.write('<input type="file" name="filetoupload"><br>');
-//   res.write('<input type="submit">');
-//   res.write('</form>');
-//   return res.end();
-// }).listen(8080);
---
+++ demo_mail.js
@@ -0,0 +1,24 @@
+var nodemailer = require('nodemailer');
+
+var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+
+    }
+
+});
+
+var mailOptions = {
+    
+
+    subject: 'Test',
+    text: 'Easy'
+}
+
+transporter.sendMail(mailOptions, function(error, info){
+    if(error){
+        console.log(error)
+    } else {
+        console.log('Email sent: ' + info.response);
+    }
+});
